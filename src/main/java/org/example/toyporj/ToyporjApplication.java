26c74e676e7016ca881730ffd44223ae44b0b5df
swan <ian@urock.kr>
2025-03-10 11:27:14 +0900

    [init] project init project

---
+++ src/main/java/org/example/toyporj/ToyporjApplication.java
@@ -0,0 +1,13 @@
+package org.example.toyporj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyporjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyporjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/toyporj/ToyporjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.toyporj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyporjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
